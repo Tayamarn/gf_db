15061204ec3cf803d969a648c6071d452f2d4e8e
Veresk <ZLveresk@gmail.com>
2023-02-18 16:15:47 +0300

    Пытаемся сделать профиль

--- registration/users/models.py
+++ registration/users/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 User = get_user_model()
 
 class Profile(models.Model):
-    user=models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    user=models.OneToOneField(User, null=False, on_delete=models.CASCADE)
     telegram = models.CharField(max_length=50, null=True, blank=True)
     vk = models.CharField(max_length=50, null=True, blank=True)
     email = models.EmailField(max_length=50, null=True, blank=True)
@@ -13,3 +15,9 @@ class Profile(models.Model):
 
     def __str__(self):
         return str(self.user)
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    print(instance)
+    if created:
+        Profile.objects.create(user=instance)
